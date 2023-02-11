fc55f39473f32a6898b006c33ff185db6d38de40
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2023-02-11 23:45:46 +0300

    Update main.go

--- http/go-fiber/body-parser/main.go
+++ http/go-fiber/body-parser/main.go
@@ -12,7 +12,7 @@ func main() {
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx){
 	  c.SendFile("index.html")
-	}
+	})
 	app.Post("/giris", func(c *fiber.Ctx) {
 	  isim := c.FormValue("isim")
 	  soyisim := c.FormValue("soyisim")
